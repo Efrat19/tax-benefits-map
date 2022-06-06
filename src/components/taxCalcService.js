07262862ed05644871ff4ae4303610ef00dd4095
Efrat19 <efrat890089@gmail.com>
2022-06-06 23:07:50 +0300

    finish logic

---
+++ src/components/taxCalcService.js
@@ -0,0 +1,19 @@
+
+// https://protocol.co.il/beneficiary-settlement/
+import cityTaxArray from './cityTax2022.json'
+
+export default class TaxCalcService {
+
+    constructor(income) {
+        this.income = income
+        this.cityTaxArray = cityTaxArray
+    }
+    calcMonthlyBenefitBy(cityTax) {
+        const benefitPercent = cityTax.benefitRate / 100
+        const annualIncome = this.income * 12
+        const incomeForBenefit = min(annualIncome, cityTax.benefitCeiling)
+        const benefit = incomeForBenefit * benefitPercent
+        const monthlyBenefit = benefit / 12
+        return monthlyBenefit
+    }
+}
